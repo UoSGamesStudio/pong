a6897a060ec94c4ae2eb248cee8dea7505b8a95c
Oluwatamilore Adebayo <80163483+adeba-io@users.noreply.github.com>
2021-06-03 18:11:15 +0100

    Made the paddle deceleration no longer overshoot

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -5,18 +5,28 @@ export(float, 0.0, 10.0) var speed: float = 5
 export(float, 0.0, 10.0) var acceleration: float = 2
 export(float, 0.1, 5.0) var deceleration_multiplier := 2.0
 
-onready var input := $Input
+onready var input : InputNode = $Input
 
 var velocity := Vector2()
 
 func _process(delta: float) -> void:
-	var c = input.get_control()
+	var c := input.get_control()
 	
+	var old_vel := velocity
+	
+	# We are decelerating
 	if c == Vector2():
 		c = -velocity.normalized() * deceleration_multiplier
-		
 
-	velocity += c * acceleration * delta
+		velocity += c * acceleration * delta
+		
+		# If this is the case, that means we have overshot durine
+		if sign(old_vel.x) != sign(velocity.x) && sign(old_vel.y) != sign(velocity.y):
+			velocity = Vector2()
+	else:
+		velocity += c * acceleration * delta
+	
+	
 	velocity.x = clamp(velocity.x, -speed, speed)
 	velocity.y = clamp(velocity.y, -speed, speed)
 	
